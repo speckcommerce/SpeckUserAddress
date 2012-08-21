1a1aceb9434883e5d610f52c10f0ee81b37bf5ad
Ben Youngblood <bx.youngblood@gmail.com>
2012-08-21 13:43:48 -0700

    update schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `user_addresses` (
-  `user_id` int(11) NOT NULL,
-  `address_id` int(11) NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `address_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`user_id`,`address_id`),
   KEY `user_id` (`user_id`),
   KEY `address_id` (`address_id`)
